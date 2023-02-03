ab09cec2905dd67192e0f9bb1a7e7179bf97c90c
isabelprado <iaptny@gmail.com>
2023-02-02 17:58:24 -0800

    fixed efficiency issues, need to finish comments

--- src/Square.java
+++ src/Square.java
@@ -1,3 +1,6 @@
+import javax.swing.*;
+import java.awt.*;
+
 /**
  * A class written to support the TicTacToe Game.
  *
@@ -11,11 +14,12 @@
  */
 
 public class Square {
-
     private String marker;
     private int row;
     private int col;
     private boolean isWinningSquare;
+    /** Graphics Window**/
+    private TicTacToeViewer window;
 
     /**
      * Constructor to initialize one Square of the
@@ -23,10 +27,10 @@ public class Square {
      * @param row the row the square is in
      * @param col the column the square is in
      */
-    public Square(int row, int col) {
+    public Square(int row, int col, TicTacToeViewer window) {
+        this.window = window;
         this.row = row;
         this.col = col;
-
         this.marker = TicTacToe.BLANK;
         this.isWinningSquare = false;
     }
@@ -50,6 +54,30 @@ public class Square {
      */
     public boolean isEmpty() {
         return this.marker.equals(TicTacToe.BLANK);
+    }
+
+    /**
+     * Calculates the dimensions of the square and draws border
+     * If not empty, draws the image and if it is a winning square
+     * makes it green
+     * @param g graphics object to draw on window
+     */
+    public void draw(Graphics g) {
+        g.setColor(Color.black);
+        int x = (window.BOARD_WIDTH / 3 * (row + 1)),
+            y = (window.BOARD_WIDTH / 3 * (col + 1));
+        int width = window.BOARD_WIDTH / 3,
+            height = window.BOARD_HEIGHT / 3;
+        g.drawRect(x, y, width, height);
+        if (isEmpty()) return;
+        if(isWinningSquare) {
+            g.setColor(Color.green);
+            g.fillRect(x, y, width, height);
+        }
+
+        Image icon = marker.equals("X") ? new ImageIcon(window.getIcons()[0]).getImage()
+                : new ImageIcon(window.getIcons()[1]).getImage();
+        g.drawImage(icon, x, y, width, height, window);
     }
 
     /**
--- src/TicTacToeViewer.java
+++ src/TicTacToeViewer.java
@@ -1,3 +1,75 @@
-public class TicTacToeViewer {
-    // TODO: Complete this class
+import javax.swing.*;
+import java.awt.*;
+
+public class TicTacToeViewer extends JFrame {
+    /** Backend **/
+    private TicTacToe backend;
+    /** Window Dimensions **/
+    public final int WINDOW_WIDTH = 800,
+                      WINDOW_HEIGHT = 800;
+    /** Board Dimensions **/
+    public final int BOARD_WIDTH = (int) (WINDOW_WIDTH * 0.60),
+                      BOARD_HEIGHT = (int) (WINDOW_HEIGHT * 0.60);
+    /** Images **/
+    private Image[] icons;
+
+    public TicTacToeViewer(TicTacToe backend) {
+        this.backend = backend;
+
+        icons = new Image[2];
+        icons[0] = new ImageIcon("Resources/isabel.png").getImage();
+        icons[1] = new ImageIcon("Resources/taylor.png").getImage();
+
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        this.setTitle("TicTacToe");
+        this.setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
+        this.setVisible(true);
+    }
+
+    public Image[] getIcons() {
+        return icons;
+    }
+
+    public void paint(Graphics g) {
+        g.setColor(Color.white);
+        g.fillRect(0, 0, WINDOW_WIDTH, WINDOW_HEIGHT);
+        drawNums(g);
+
+        Square[][] squares = backend.getBoard();
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                squares[i][j].draw(g);
+            }
+        }
+
+        if (backend.getGameOver()) {
+            writeWinner(g);
+        }
+    }
+
+    public void drawNums(Graphics g) {
+        g.setColor(Color.red);
+        g.setFont(new Font("Serif", Font.PLAIN, 50));
+        int x_col, y_col, x_row, y_row;
+
+        // Print columns
+        x_col = ((WINDOW_WIDTH - BOARD_WIDTH) / 2) + (BOARD_WIDTH / 6) - 10;
+        y_col = (WINDOW_HEIGHT - BOARD_HEIGHT) / 2 - 40;
+        x_row = (WINDOW_WIDTH - BOARD_WIDTH) / 4;
+        y_row = ((WINDOW_HEIGHT - BOARD_HEIGHT) / 2) + (BOARD_HEIGHT / 6) + 10;
+        for (int i = 0; i < 3; i++) {
+            g.drawString(String.valueOf(i), x_col, y_col);
+            x_col += BOARD_WIDTH / 3;
+
+            g.drawString(String.valueOf(i), x_row, y_row);
+            y_row += BOARD_WIDTH / 3;
+        }
+    }
+
+    public void writeWinner(Graphics g) {
+        g.setColor(Color.black);
+        g.setFont(new Font("Serif", Font.BOLD, 100));
+        String output = backend.checkTie() ? "It's a tie!" : backend.getWinner() + " Wins!";
+        g.drawString(output, WINDOW_WIDTH / 4, WINDOW_HEIGHT - (WINDOW_HEIGHT - BOARD_HEIGHT) / 4);
+    }
 }
